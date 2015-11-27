50b352f9b19ffe5a480d1633193157e01c272e90
Faris <farishassan2012@gmail.com>
2015-11-28 04:05:44 +0800

    Additions
    Enhancements to the evaluation function to make it heuristic.

--- try.py
+++ try.py
@@ -5,54 +5,50 @@ import util
 
 class WaterJugProblem:
     def __init__(self):
-        self.capacites = (4, 3)
-        self.statesCount =1
+        self.capacities = (4, 3) # the size of the jugs stored in tuple
+
 
     def startState(self):
         return (0, 0)
 
-    def isGoal(self, state):
+    def isGoal(self, state): # it will test if a certain state is goal state or not
         return state[0] == 2
 
     def getSuccessors(self, (J1, J2)):  # returns	list	of	successors	to	state
-        successors = []
-        (C1, C2) = self.capacites
-        if(J1 < C1):  successors.append(((C1, J2),'Fill J1', 1))
+        successors = [] # list
+        (C1, C2) = self.capacities
+        if(J1 < C1):  successors.append(((C1, J2),'Fill J1', 1)) # the possible operations or moves
         if(J2 < C2):  successors.append(((J1, C2),'Fill J2', 1))
         if J1 > 0:    successors.append(((0, J2),'Dump J1', 1))
         if J2 > 0:    successors.append(((J1, 0),'Dump J2', 1))
-        if J2 < C2 and J1 > 0:
-            delta = min(J1, C2 - J2)
-            successors.append(((J1 - delta, J2 + delta),'Pour J1 into J2', 1))
-        if J1 < C1 and J2 > 0:
-            delta = min(J2, C1 - J1)
-            successors.append(((J1 + delta, J2 - delta),'pour J2 into J1', 1))
 
-        self.statesCount += len(successors)
+        if J1+J2 <= 3:
+            alpha=J1+J2
+            successors.append(((0,alpha),'Dump J1 into J2',1))
+        if J1+J2 <= 4:
+            alpha=J1+J2
+            successors.append(((alpha,0),'Dump J2 into J1',1))
+        if J1+J2 > 4:
+            alpha = J1+J2-4
+            successors.append(((C1,alpha),'Fill J1 from J2',1))
+        if J1+J2 > 3:
+            alpha = J1+J2-3
+            successors.append(((alpha,C2),'Fill J2 from J1',1))
+
         return successors
 
 
-class Node():
-    """
-    A container storing the current state of a node, the list
-    of  directions that need to be followed from the start state to
-    get to the current state and the specific problem in which the
-    node will be used.
-    """
+def waterHeurestic(state, problem=None):
+    return abs(state[0]-2)
 
+
+class Node():
     def __init__(self, state, path, cost=0, heuristic=0, problem=None):
         self.state = state
         self.path = path
         self.cost = cost
         self.heuristic = heuristic
         self.problem = problem
-
-    def __str__(self):
-        string = "Current State: "
-        string += self.__str__(self.state)
-        string += "\n"
-        string == "Path: " + self.path + "\n"
-        return string
 
     def getSuccessors(self, heuristicFunction=None):
         children = []
@@ -70,10 +66,6 @@ class Node():
         return children
 
 def nullHeuristic(state, problem=None):
-    """
-    A heuristic function estimates the cost from the current state to the nearest
-    goal in the provided SearchProblem.  This heuristic is trivial.
-    """
     return 0
 
 
@@ -81,29 +73,36 @@ def aStarSearch(problem, heuristic=nullHeuristic):
     "Search the node that has the lowest combined cost and heuristic first."
 
     closed = set()
-    fringe = util.PriorityQueue()
+    Q = util.PriorityQueue()
 
     startNode = Node(problem.startState(), [], 0, 0, problem)
-    fringe.push(startNode, startNode.cost + startNode.heuristic)
-
+    Q.push(startNode, startNode.cost + startNode.heuristic)
+    visited = 0
     while True:
-        if fringe.isEmpty():
+        if Q.isEmpty():
             return False
-        node = fringe.pop()
+        node = Q.pop()
+        visited +=1
         if problem.isGoal(node.state):
+            print 'expanded',len(closed)
             return node.path
         if node.state not in closed:
             closed.add(node.state)
             for childNode in node.getSuccessors(heuristic):
-                fringe.push(childNode, childNode.cost + childNode.heuristic)
+                Q.push(childNode, childNode.cost + childNode.heuristic)
+
+
 
 
 
 def test():
     problem = WaterJugProblem()
-    path = aStarSearch(problem)
+    print 'waterHeurestic'
+    path = aStarSearch(problem,waterHeurestic)
+    print 'nullHeuristic'
+    path = aStarSearch(problem,nullHeuristic)
     print path
-    print problem.statesCount
+
 
 
 test()
